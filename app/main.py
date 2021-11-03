4748084d6a07a2c9c3080664d5cf27bbf3a5a9e5
Robel-robel <robelberhanu89@gmail.com>
2021-11-03 17:24:41 +0200

    7 - FastAPI Git & pre-commit

--- app/main.py
+++ app/main.py
@@ -11,5 +11,8 @@ templates = Jinja2Templates(directory=str(BASE_DIR/ "templates"))
 
 @app.get("/", response_class=HTMLResponse)
 def home_view(request: Request):
-
     return templates.TemplateResponse("home.html", {"request": request})
+
+@app.post("/") #http POST
+def home_detail_view():
+    return {"hello": "world"}
